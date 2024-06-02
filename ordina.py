692ebc8cfa1f5ac2da2dc5ac6850a1dcb7da0bb6
DrCocco <federicoroncaglia@gmail.com>
2024-06-02 17:39:35 +0200

    inizia a prendere forma

--- ordina.py
+++ ordina.py
@@ -1,5 +1,16 @@
 from pathlib import Path
-for path in Path('c:/Users/ITFERON/OneDrive - ABB/python/ordensomeanse').rglob('*'):
-#    print(path.suffix)
-    if not path.is_dir:
-        print(path)
+from PIL import ExifTags, Image
+
+for path in Path('./').rglob('*'):
+  #  print(path)
+    if path.suffix == ".jpg":
+        try:
+            img = Image.open (path)     
+            exif = img.getexif() 
+            shootdate = exif.get(306) #found 36867 in interner - 306 on my linux setup
+            #print(exif)  # print all exif field to check where date is stored (after i will comment it)
+            if not shootdate is None: #check if shootdata is valid
+                print(shootdate)
+
+        except:
+            print("problemi problemi problemi")
